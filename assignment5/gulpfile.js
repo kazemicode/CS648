fe78c83c8dd9f4c2f50b1e18fa423ca6fc14a8cb
kazemicode <sarieses@gmail.com>
2020-10-03 20:16:37 -0700

    initialize assignment 5

---
+++ assignment5/gulpfile.js
@@ -0,0 +1,11 @@
+var {src, dest} = require('gulp');
+var sass = require('gulp-sass');
+sass.compuler = require('node-sass');
+
+function css() {
+    return src('src/*.scss')
+        .pipe(sass())
+        .pipe(dest('dist/css'))
+}
+
+exports.default = css;
