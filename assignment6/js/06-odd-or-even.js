23b188ebef9a70589d2018d8ce81dd405f8a2699
kazemicode <sarieses@gmail.com>
2020-10-08 22:39:25 -0700

    formatting

--- assignment6/js/06-odd-or-even.js
+++ assignment6/js/06-odd-or-even.js
@@ -1,5 +1,5 @@
 for(var i = 0; i <= 15; i++){
-    if(i%2==0){
+    if(i%2 === 0){
         console.log(i + " is even");
     }
     else {
--- assignment6/js/07-marco-polo.js
+++ assignment6/js/07-marco-polo.js
@@ -1,13 +1,13 @@
 for(var i = 1; i < 101; i++){
-    if(i%3 == 0){
-        if(i%5 == 0){
+    if(i%3 === 0){
+        if(i%5 === 0){
             console.log("Marco! Polo!");
         }
         else {
             console.log("Marco!");
         }
     }
-    else if(i%5 ==0){
+    else if(i%5 === 0){
         console.log("Polo!");
     }
     else {
