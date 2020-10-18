d8f68ef6e7218278cc71c09f7bb4c8cdd7ef5967
kazemicode <sarieses@gmail.com>
2020-10-18 09:27:54 -0700

    finished step 5

--- assignment7/part5/js/dom-practice.js
+++ assignment7/part5/js/dom-practice.js
@@ -1,12 +1,37 @@
-/*eslint-env browser*/
 
 //STEP 1
+function beenClicked(){
+    alert("I've been clicked!");
+}
 
 //STEP 2
+var myButton = document.getElementById("myButton");
+myButton.onclick =  function () {
+    "use strict";
+    window.alert("I've been clicked!");
+};
+
 
 //STEP 3
+var $ = function (id) {
+    "use strict";
+    return document.getElementById(id);
+};
+$("anotherButton").addEventListener("click", beenClicked);
 
 //STEP 4
 
+$("anotherButton").addEventListener("click", function () {
+    "use strict";
+    window.alert("I've been clicked!");
+});
+
 //STEP 5
+
+
+// wait for page to load to use DOM to get element by id
+window.addEventListener("load", function () {
+    "use strict";
+    $("anotherButton").addEventListener("click", beenClicked);
+});
 
