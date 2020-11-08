7cc5efbcb23553b480296a2285083afb67b791ff
kazemicode <sarieses@gmail.com>
2020-11-07 23:43:17 -0800

    move tooltip scripts into external js since it is used by each page

---
+++ assignment10/vectacorp/js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $('#responsive-menu').mobileMenu();
+    // tooltips for navbar
+    $('nav ul li a').prop('title','navItem'); 
+
+    $('nav ul li a:first').tooltip({
+        content : "Return home"
+    })
+
+    $('nav ul li a').eq(1).tooltip({
+        content : "Learn more all about VectaCorp"
+    })
+
+    $('nav ul li a').eq(2).tooltip({
+        content : "Vecta Corp. offers 3 solutions for prospecting, converting, and retaining customers."
+    })
+
+    $('nav ul li a').eq(3).tooltip({
+        content : "Access technical resources and documentation"
+    })
+
+    $('nav ul li a:last').tooltip({
+        content : "Contact us for additional information"
+    })
+});
