572747b6897f71c2006ee9d1e1ffdbc5da187f24
kazemicode <sarieses@gmail.com>
2020-11-08 18:34:21 -0800

    convert elements on contactus to jQuery UI versions

--- assignment10/vectacorp/js/scripts.js
+++ assignment10/vectacorp/js/scripts.js
@@ -34,4 +34,41 @@ $(document).ready(function(){
     // tabs for management team section (aboutus)
     $("#management").tabs().addClass("ui-tabs-vertical")
 
+    // sticky navbar (solutions)
+    $("#sticky-nav").sticky({topSpacing:0});
+
+    // scroll to top (solutions)
+    $(".top").on('click', function(){
+        $.smoothScroll()
+        return false
+    })
+    
+    // Quick nav links (solutions)
+    $('#solution-1-link').on('click', function () {
+        $.smoothScroll({
+          scrollTarget: '#solution-1'
+        })
+        return false
+      })
+      $('#solution-2-link').on('click', function () {
+        $.smoothScroll({
+          scrollTarget: '#solution-2'
+        })
+        return false
+      })
+      $('#solution-3-link').on('click', function () {
+        $.smoothScroll({
+          scrollTarget: '#solution-3'
+        })
+        return false
+      })
+
+      // Convert all checkboxes and radios to JQuery UI versions (contactus) 
+      $("input[type=checkbox], input[type=radio]").checkboxradio()
+      // Convert dropdown to JQuery UI version (contactus)
+      $("[name='companysize']").selectmenu()
+      // Convert submit button to JQuery UI version (contactus)
+      $("[name='submit']").button()
+    
+
 });
